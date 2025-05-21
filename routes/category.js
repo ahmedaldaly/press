4d8c062b1137cbf196498f98503df2ca9f2d2be9
ahmedaldaly <ranaandahmed55@gmail.com>
2025-05-21 04:47:30 -0700

    token

--- routes/category.js
+++ routes/category.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const {createCategory ,getAllCategory ,getCategory ,removeCategory} = require('../controller/categoryController');
+const { authrization, Admin } = require('../maddelware/authrazition');
 
-router.route('/').post(createCategory).get(getAllCategory)
-router.route('/:id').delete(removeCategory).get(getCategory)
+router.route('/').post(authrization,Admin,createCategory).get(getAllCategory)
+router.route('/:id').delete(authrization,Admin,removeCategory).get(getCategory)
 module.exports = router
